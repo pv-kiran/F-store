97d5caa8a2166044ba52418e4149568d5f30bf62
pv-kiran <kiranpv903@gmail.com>
2023-02-14 20:37:31 +0530

    port change

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.get('*' , (req,res) => {
   res.render('404');
 })
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
